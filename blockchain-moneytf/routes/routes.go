d5abf032ab83a2ea4b28281ac7702341f11d6c35
Jonathan1366 <s32220020@student.ubm.ac.id>
2024-11-08 15:39:24 +0700

    update blockchain2

--- blockchain-moneytf/routes/routes.go
+++ blockchain-moneytf/routes/routes.go
@@ -9,7 +9,7 @@ func SetupRoutes(app *fiber.App, authhandler *handlers.AuthHandlers) {
 	api:=app.Group("/api")
 	api.Post("/transaction", authhandler.CreateTransactionHandler)
 	api.Get("/transaction/:id", authhandler.GetTransactionHandler)
-	api.Get("/transaction", authhandler.GetAllTransactionHandler)
+	api.Get("/transactions", authhandler.GetAllTransactionHandler)
 	api.Put("/transaction/:id", authhandler.UpdateTransactionsHandler)
 	api.Delete("/transaction/:id", authhandler.DeleteTransactionHandler)
 	api.Get("/blocks", authhandler.GetAllBlocksHandler)
