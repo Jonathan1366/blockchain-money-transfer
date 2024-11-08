687a9842ee49c6392123bd95865885f49408592d
Jonathan1366 <s32220020@student.ubm.ac.id>
2024-11-08 16:33:27 +0700

    update terbaru nov5

--- blockchain-moneytf/db/connection.go
+++ blockchain-moneytf/db/connection.go
@@ -2,30 +2,38 @@ package db
 
 import (
 	"context"
+	"log"
 	"os"
 
-	"github.com/gofiber/fiber/v2/log"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/joho/godotenv"
 )
 
-func Connect() *pgx.Conn {
-	if os.Getenv("RAILWAY_ENVIRONMENT")==""{
-		err:=godotenv.Load()
+func Connect() *pgxpool.Pool {
+	// Load .env file jika tidak berjalan di Railway
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		err := godotenv.Load()
 		if err != nil {
-			log.Fatalf("No .env file found, proceeding with environment variables")
+			log.Println("No .env file found, proceeding with environment variables")
 		}
 	}
-	
-	databaseURL:=os.Getenv("DATABASE_URL")
-	if databaseURL==""{
+
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
 		log.Fatal("DATABASE_URL is not set in the environment")
 	}
-	conn, err:=pgx.Connect(context.Background(), databaseURL)
+
+	// Konfigurasi pool koneksi
+	config, err := pgxpool.ParseConfig(databaseURL)
 	if err != nil {
-		log.Fatalf("Unable to connect database")
+		log.Fatalf("Unable to parse DATABASE_URL: %v", err)
 	}
+
+	// Buat pool koneksi
+	conn, err := pgxpool.ConnectConfig(context.Background(), config)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+
 	return conn
 }
-
-
