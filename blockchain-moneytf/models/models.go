28ae50581dacd559e352764571b529d1a6fb7ad9
Jonathan1366 <s32220020@student.ubm.ac.id>
2024-11-09 01:10:43 +0700

    update create transaction3

--- blockchain-moneytf/models/models.go
+++ blockchain-moneytf/models/models.go
@@ -1,28 +1,28 @@
-	package models
+package models
 
-	type User struct {
-		ID         int     `json:"id"`
-		Name       string  `json:"name"`
-		Balance    float64 `json:"balance"`
-		PublicKey  string  `json:"public_key"`
-		PrivateKey string  `json:"-"`
-	}
+type User struct {
+	ID         int     `json:"id"`
+	Name       string  `json:"name"`
+	Balance    float64 `json:"balance"`
+	PublicKey  string  `json:"public_key"`
+	PrivateKey string  `json:"-"`
+}
 
-	type Transaction struct {
-		ID              int     `json:"id"`
-		SenderID        int     `json:"sender_id"`
-		ReceiverID      int     `json:"receiver_id"`
-		Amount          float64 `json:"amount"`
-		Signature       string  `json:"signature"`
-		TransactionHash string  `json:"transaction_hash"`
-		Waktu           string  `json:"waktu"`
-	}
+type Transaction struct {
+	ID              int     `json:"id"`
+	SenderID        int     `json:"sender_id"`
+	ReceiverID      int     `json:"receiver_id"`
+	Amount          float64 `json:"amount"`
+	Signature       string  `json:"signature"`
+	TransactionHash string  `json:"transaction_hash"`
+	Waktu           string  `json:"waktu"`
+}
 
-	type Block struct {
-		Id            int    `json:"id"`
-		TransactionId int    `json:"transaction_id"`
-		PreviousHash  string `json:"previous_hash"`
-		Hash          string `json:"hash"`
-		Nonce         int    `json:"nonce"`
-		Timestamp     string `json:"timestamp"`
-	}
+type Block struct {
+	Id            int    `json:"id"`
+	TransactionId int    `json:"transaction_id"`
+	PreviousHash  string `json:"previoush_hash"`
+	Hash          string `json:"hash"`
+	Nonce         int    `json:"nonce"`
+	Timestamp     string `json:"timestamp"`
+}
